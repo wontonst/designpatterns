8be5a88d767c7bd096e4d6c1a00d783bca300ca8
Roy Zheng <roycraft3@gmail.com>
2013-07-15 17:54:56 -0700

    initial

---
+++ src/wontonst/tankfactory/interfaces/TankPart.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wontonst.tankfactory.interfaces;
+
+/**
+ *
+ * @author rzheng
+ */
+public interface TankPart {
+    
+}
---
+++ src/wontonst/tankfactory/tankparts/turret/CommanderMachineGun.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wontonst.tankfactory.tankparts.turret;
+
+/**
+ *
+ * @author rzheng
+ */
+public class CommanderMachineGun {
+    
+}

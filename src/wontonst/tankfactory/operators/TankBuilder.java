0054669c30929722a42cb5d738b33848e4782a00
Roy Zheng <roycraft3@gmail.com>
2013-07-18 11:44:48 -0700

    mv

---
+++ src/wontonst/tankfactory/operators/TankBuilder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wontonst.tankfactory.operators;
+
+import wontonst.tankfactory.generic.TankFactory;
+import wontonst.tankfactory.tankparts.generic.Tank;
+import wontonst.tankfactory.tankparts.turret.Turret;
+
+/**
+ *
+ * @author rzheng
+ */
+public class TankBuilder {
+    
+    public static Tank buildTank(TankFactory f){
+        Turret turret = f.buildTurret();
+        Chassis chassis  =f.buildChassis(eng);
+    }
+}

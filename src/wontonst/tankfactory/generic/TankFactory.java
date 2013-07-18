95c67843b3962a10fe2f6c7dfdbf002f175bf8f6
Roy Zheng <roycraft3@gmail.com>
2013-07-18 11:44:39 -0700

    more code

---
+++ src/wontonst/tankfactory/generic/TankFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wontonst.tankfactory.generic;
+
+import wontonst.tankfactory.tankparts.generic.Chassis;
+import wontonst.tankfactory.tankparts.generic.Engine;
+import wontonst.tankfactory.tankparts.generic.Tank;
+import wontonst.tankfactory.tankparts.turret.Turret;
+
+/**
+ * Abstract factory for creating TankPart objects. Design goals: - top-level
+ * tank factory system is independent of specialized tank manufacturing
+ * techniques - unified top-level system for configuration of tanks - future
+ * repair system should be able to manipulate any tank through single interface
+ * - a tank is a tank is a tank. internal tank construction details should
+ * remain secret
+ *
+ * @author wontonst
+ */
+public abstract class TankFactory {
+
+    protected TurretAssembler turret_assembler;
+    protected ChassisAssembler chassis_assembler;
+
+    protected TankFactory(TurretAssembler ta, ChassisAssembler ca) {
+        this.chassis_assembler = ca;
+        this.turret_assembler = ta;
+    }
+
+    public Turret buildTurret();
+
+    public Chassis buildChassis(Engine e);
+
+    public Tank buildTank(Turret t, Chassis c);
+}
--- src/wontonst/tankfactory/generic/TankPart.java
+++ src/wontonst/tankfactory/generic/TankPart.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package wontonst.tankfactory.interfaces;
+package wontonst.tankfactory.generic;
 
 /**
  *
  * @author rzheng
  */
 public interface TankPart {
-    
+    public String getConfiguration();
 }
